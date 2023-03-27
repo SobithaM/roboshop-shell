2e19b1f84d2e6839551a33a42a647fab0d6c1146
sobitha <sobitham97@gmail.com>
2023-03-27 19:46:23 +0530

    frontend sub

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,4 @@
-code-dir=${pwd}
+code-dir=$(pwd)
 echo -e "\e[35minstalling nginx \e[0m"
 yum install nginx -y
 echo -e "\e[35mremoving old content  \e[0m"
@@ -9,7 +9,7 @@ echo -e "\e[35mextracting downloded content \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 echo -e "\e[35mcoping nginx config for roboshop \e[0m"
-cp ${code-dir} /configs/ngnix-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code-dir}/configs/ngnix-roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[35menabling nginx \e[0m"
 systemctl enable nginx
 echo -e "\e[35mrestart nginx \e[0m"
